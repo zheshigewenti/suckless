814ac0732b47a8d2743663de3be97db9777f7e33
zheshigewenti <dzn1534564656@gmail.com>
2023-09-06 13:09:59 +0800

    update

--- dwm/config.h
+++ dwm/config.h
@@ -11,16 +11,16 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
 /* appearance */
-static unsigned int borderpx  = 0;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx  = 0;              /* border pixel of windows */
+static unsigned int snap      = 32;             /* snap pixel */
+static unsigned int gappih    = 10;             /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;             /* vert inner gap between windows */
+static unsigned int gappoh    = 10;             /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 10;             /* vert outer gap between windows and screen edge */
+static int swallowfloating    = 0;              /* 1 means swallow floating windows by default */
+static int smartgaps          = 0;              /* 1 means no outer gap when there is only one window */
+static int showbar            = 1;              /* 0 means no bar */
+static int topbar             = 1;              /* 0 means bottom bar */
 static char *fonts[]          = {"Liberation Mono:pixelsize=10","WenQuanYi Micro Hei Mono:size=9","SauceCodePro Nerd Font Mono:size=16"};
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#222222";
@@ -74,19 +74,6 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",	tile },			/* Default: Master on left, slaves on right */
-	{ "TTT",	bstack },		/* Master on top, slaves on bottom */
-
-
-	{ "[@]",	spiral },		/* Fibonacci spiral */
-	{ "[\\]",	dwindle },		/* Decreasing in size right and leftward */
-
-	{ "[D]",	deck },			/* Master on left, slaves in monocle-like mode on right */
-	{ "[M]",	monocle },		/* All windows on top of eachother */
-
-	{ "|M|",	centeredmaster },		/* Master in middle, slaves on sides */
-	{ ">M>",	centeredfloatingmaster },	/* Same but master floats */
-
-	{ "><>",	NULL },			/* no layout function means floating behavior */
 	{ NULL,		NULL },
 };
 
@@ -108,30 +95,6 @@ static const Layout layouts[] = {
 /* commands */
 static const char *termcmd[]  = { TERMINAL, NULL };
 
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-		{ "color0",		STRING,	&normbordercolor },
-		{ "color8",		STRING,	&selbordercolor },
-		{ "color0",		STRING,	&normbgcolor },
-		{ "color4",		STRING,	&normfgcolor },
-		{ "color0",		STRING,	&selfgcolor },
-		{ "color4",		STRING,	&selbgcolor },
-		{ "borderpx",		INTEGER, &borderpx },
-		{ "snap",		INTEGER, &snap },
-		{ "showbar",		INTEGER, &showbar },
-		{ "topbar",		INTEGER, &topbar },
-		{ "nmaster",		INTEGER, &nmaster },
-		{ "resizehints",	INTEGER, &resizehints },
-		{ "mfact",		FLOAT,	&mfact },
-		{ "gappih",		INTEGER, &gappih },
-		{ "gappiv",		INTEGER, &gappiv },
-		{ "gappoh",		INTEGER, &gappoh },
-		{ "gappov",		INTEGER, &gappov },
-		{ "swallowfloating",	INTEGER, &swallowfloating },
-		{ "smartgaps",		INTEGER, &smartgaps },
-};
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
@@ -160,37 +123,18 @@ static const Key keys[] = {
 
 
 	{ MODKEY,XK_c,spawn,{.v = (const char*[]){ BROWSER, NULL } } },
-	/* { MODKEY,XK_e,spawn,SHCMD(TERMINAL " -e neomutt ") }, */
-	/* { MODKEY,XK_E,spawn,SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") }, */
-	/* { MODKEY,XK_t,setlayout,{.v = &layouts[0]} }, /1* tile *1/ */
-	/* { MODKEY,XK_T,setlayout,{.v = &layouts[1]} }, /1* bstack *1/ */ 
-	/* { MODKEY,XK_y,setlayout,	{.v = &layouts[2]} }, /1* spiral *1/ */
-	// { MODKEY,XK_Y,setlayout,	{.v = &layouts[3]} }, /* dwindle */
-	// { MODKEY,XK_u,setlayout,{.v = &layouts[4]} }, /* deck */
-	// { MODKEY,XK_U,setlayout,{.v = &layouts[5]} }, /* monocle */
-	// { MODKEY,XK_i,setlayout,{.v = &layouts[6]} }, /* centeredmaster */
-	// { MODKEY,XK_I,setlayout,{.v = &layouts[7]} }, /* centeredfloatingmaster */
 	{ MODKEY,XK_a,togglegaps,	{0} },
 	{ MODKEY,XK_d,spawn,{.v = (const char*[]){ "dmenu_run", NULL } } },
 	{ MODKEY,XK_f,togglefullscr,{0} },
 	{ MODKEY,XK_Return,	spawn,{.v = termcmd } },
 	{ MODKEY,XK_b,togglebar,{0} },
-	/* { MODKEY,XK_n,spawn,{.v = (const char*[]){ TERMINAL, "-e", "nvim", NULL } } }, */
 	/* { MODKEY,XK_N,spawn,SHCMD(TERMINAL " -e newsboat ") }, */
 
 
 	{ 0,XK_F1,spawn,{.v = mutevol }},
 	{ 0,XK_F2,spawn,{.v = downvol }},
 	{ 0,XK_F3,spawn,{.v = upvol   }},
-	{ 0,XK_F4,spawn,SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
-	/* { MODKEY,XK_F5, xrdb, {.v = NULL } }, */
-	/* { MODKEY,XK_F6,spawn,{.v =  (const char*[]){ "torwrap", NULL } } }, */
-	/* { MODKEY,XK_F7,spawn,{.v =  (const char*[]){ "td-toggle", NULL } } }, */
-	/* { MODKEY,XK_F8,spawn,{.v =  (const char*[]){ "mailsync", NULL } } }, */
-	/* { MODKEY,XK_F9,spawn,{.v =  (const char*[]){ "mounter", NULL } } }, */
-	/* { MODKEY,XK_F10,spawn,{.v = (const char*[]){ "unmounter", NULL } } }, */
-	/* { MODKEY,XK_F11,spawn,}, */
-	/* { MODKEY,XK_F12,		spawn,		SHCMD("remaps") }, */
+	{ 0,XK_F11,spawn,SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
 
 
 	{ MODKEY,XK_Left,setmfact, {.f = -0.05} },
@@ -210,16 +154,39 @@ static const Key keys[] = {
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	/* { ClkStatusText,        0,              Button1,        spawn,          SHCMD(TERMINAL     " -e alsamixer") }, */
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        defaultgaps,    {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkClientWin,      		MODKEY,		      Button4,	      incrgaps,	{.i = +1} },
-	{ ClkClientWin,      		MODKEY,		      Button5,	      incrgaps,	{.i = -1} },
+	{ ClkClientWin,         ControlMask,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         ControlMask,         Button3,        resizemouse,    {0} },
+	/* { ClkClientWin,          MODKEY,         Button2,        defaultgaps,    {0} }, */
+	/* { ClkClientWin,      		MODKEY,		      Button4,	      incrgaps,	{.i = +1} }, */
+	/* { ClkClientWin,      		MODKEY,		      Button5,	      incrgaps,	{.i = -1} }, */
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkTagBar,		        0,		          Button4,	      shiftview,{.i = -1} },
-	{ ClkTagBar,		        0,		          Button5,	      shiftview, {.i = 1} },
-	{ ClkRootWin,		        0,		          Button2,	      togglebar,      {0} },
+	/* { ClkTagBar,            0,              Button3,        toggleview,     {0} }, */
+	/* { ClkTagBar,            MODKEY,         Button1,        tag,            {0} }, */
+	/* { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} }, */
+	/* { ClkTagBar,		        0,		          Button4,	      shiftview,{.i = -1} }, */
+	/* { ClkTagBar,		        0,		          Button5,	      shiftview, {.i = 1} }, */
+	/* { ClkRootWin,		      0,		          Button2,	      togglebar,      {0} }, */
+};
+
+/* Xresources preferences to load at startup */
+ResourcePref resources[] = {
+		{ "color0",		STRING,	&normbordercolor },
+		{ "color8",		STRING,	&selbordercolor },
+		{ "color0",		STRING,	&normbgcolor },
+		{ "color4",		STRING,	&normfgcolor },
+		{ "color0",		STRING,	&selfgcolor },
+		{ "color4",		STRING,	&selbgcolor },
+		{ "borderpx",		INTEGER, &borderpx },
+		{ "snap",		INTEGER, &snap },
+		{ "showbar",		INTEGER, &showbar },
+		{ "topbar",		INTEGER, &topbar },
+		{ "nmaster",		INTEGER, &nmaster },
+		{ "resizehints",	INTEGER, &resizehints },
+		{ "mfact",		FLOAT,	&mfact },
+		{ "gappih",		INTEGER, &gappih },
+		{ "gappiv",		INTEGER, &gappiv },
+		{ "gappoh",		INTEGER, &gappoh },
+		{ "gappov",		INTEGER, &gappov },
+		{ "swallowfloating",	INTEGER, &swallowfloating },
+		{ "smartgaps",		INTEGER, &smartgaps },
 };
