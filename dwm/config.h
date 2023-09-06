2ef7aaa44f69227b3bbd9cd1b04e23c07459d4b0
zheshigewenti <dzn1534564656@gmail.com>
2023-09-06 15:32:39 +0800

    update

--- dwm/config.h
+++ dwm/config.h
@@ -13,10 +13,10 @@ static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corne
 /* appearance */
 static unsigned int borderpx  = 0;              /* border pixel of windows */
 static unsigned int snap      = 32;             /* snap pixel */
-static unsigned int gappih    = 10;             /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;             /* vert inner gap between windows */
-static unsigned int gappoh    = 10;             /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;             /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 5;             /* horiz inner gap between windows */
+static unsigned int gappiv    = 5;             /* vert inner gap between windows */
+static unsigned int gappoh    = 5;             /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 5;             /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;              /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;              /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;              /* 0 means no bar */
